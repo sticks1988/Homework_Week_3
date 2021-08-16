f2b238d04fba7012e4a7b4d94b26875d9f7cbfef
sticks1988 <mikepb6691@aol.com>
2021-08-15 20:33:46 -0400

    Adding variable confirmations prompt
    adding prompts for variables

--- Develop/script.js
+++ Develop/script.js
@@ -6,6 +6,11 @@ var specialChar = ["!", "%", "&", ",", "*", "+"];
 var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
+var confirmLength = "";
+var confirmSpecialCharacter;
+var confirmNumericCharacter;
+var confirmUpperCase;
+var confirmLowerCase;
 
 // Write password to the #password input
 function writePassword() {
